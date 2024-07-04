32cdf16f363d600a0d641a6bb9435efefc709ce7
Alex Arguelles <alexarguelles87923@gmail.com>
2024-07-04 12:29:34 -0400

    Fix Hero, Nav, and About Me

--- src/app/features/projects/projects.component.ts
+++ src/app/features/projects/projects.component.ts
@@ -12,7 +12,9 @@ export class ProjectsComponent {
       description: 'A website created for the SHPE club at Miami Dade College to welcome new members and maintain a historical record. Collaborated with Kevin Pino and Emilio Laurenti.',
       githubLink: 'Private',
       projectUrl: 'https://www.shpemdc.com',
-      techStack: ['Bootstrap', 'Flask', 'MySQL', 'Ubuntu Linux', 'Cloudflare', 'HTML', 'CSS', 'JavaScript', 'Python'],
+      hosted: 'Personal Ubuntu Server',
+      imageUrl: 'https://example.com/path_to_shpe_image.jpg',
+      techStack: ['Bootstrap', 'Flask', 'MySQL', 'Cloudflare', 'HTML', 'CSS', 'JavaScript', 'Python'],
       collaborators: [
         { name: 'Kevin Pino', portfolioUrl: 'https://kevinpino.com' },
         { name: 'Emilio Laurenti', portfolioUrl: null }
@@ -23,7 +25,9 @@ export class ProjectsComponent {
       description: 'Developed in collaboration with the Women in Tech club at Miami Dade College and Kevin Pino. The project aims to offer coding practice and web design opportunities.',
       githubLink: 'Private',
       projectUrl: 'https://www.witmdc.com',
-      techStack: ['Bootstrap', 'Express', 'JavaScript', 'React', 'Figma', 'Ubuntu Linux', 'Cloudflare', 'CSS', 'HTML', 'SQLite', 'Axios', 'GitHub'],
+      hosted: 'Personal Ubuntu Server',
+      imageUrl: 'https://example.com/path_to_wit_image.jpg',
+      techStack: ['Bootstrap', 'Express', 'JavaScript', 'React', 'Figma', 'Cloudflare', 'CSS', 'HTML', 'SQLite', 'Axios', 'GitHub'],
       collaborators: [
         { name: 'Kevin Pino', portfolioUrl: 'https://kevinpino.com' },
         { name: 'Fer Pacheco', portfolioUrl: null }
@@ -34,7 +38,9 @@ export class ProjectsComponent {
       description: 'Created with the Robotics Club at Miami Dade College to serve as an interest form and announcement page for a robot fighting event. Collaborated with Felipe Rodas, Stephanie Abantos, Kevin Pino, and Emilio Laurenti.',
       githubLink: 'https://github.com/alexarg87923/project_robotics',
       projectUrl: 'https://www.termitomator.com',
-      techStack: ['React', 'Ubuntu Linux', 'Google Domains', 'JavaScript', 'HTML', 'CSS', 'Bootstrap', 'MongoDB Atlas', 'Axios', 'GitHub'],
+      hosted: 'Heroku Cloud & MongoDB Atlas',
+      imageUrl: 'https://example.com/path_to_robotics_event_image.jpg',
+      techStack: ['React', 'Google Domains', 'JavaScript', 'HTML', 'CSS', 'Bootstrap', 'Axios', 'GitHub'],
       collaborators: [
         { name: 'Kevin Pino', portfolioUrl: 'https://kevinpino.com' },
         { name: 'Emilio Laurenti', portfolioUrl: null },
@@ -47,6 +53,8 @@ export class ProjectsComponent {
       description: 'A personal project to refine C++ object-oriented programming skills through the development of a chess engine.',
       githubLink: 'Private',
       projectUrl: 'NoURL',
+      hosted: 'Offline',
+      imageUrl: 'https://example.com/path_to_chess_engine_image.jpg',
       techStack: ['C++', 'GLOG', 'SFML', 'GitHub']
     },
     {
@@ -54,7 +62,9 @@ export class ProjectsComponent {
       description: 'An XML validator and parser developed during an internship at UKG, in collaboration with Adrian Wright, Luis Seoene, and Heidi Heid Villar.',
       githubLink: 'Private',
       projectUrl: 'Private',
-      techStack: ['C#', '.NET', 'Docker', 'Windows Server', 'XUnit Test', 'GitHub'],
+      hosted: 'Company Windows Server using IIS',
+      imageUrl: 'https://example.com/path_to_xml_validator_image.jpg',
+      techStack: ['C#', '.NET', 'Docker', 'XUnit Test', 'GitHub'],
       collaborators: [
         { name: 'Adrian Wright', portfolioUrl: null },
         { name: 'Luis Seoene', portfolioUrl: null },
