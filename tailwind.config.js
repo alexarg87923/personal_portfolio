e3c7ee9ae14cae15fadea7c85a81148c8e549fb8
Alex Arguelles <alexarguelles87923@gmail.com>
2024-07-07 03:59:00 -0400

    Fix folder structure & refactoring directories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx,html}", // Adjust according to your project structure
+    "client/src/**/*.{js,jsx,ts,tsx,html}", // Adjust according to your project structure
   ],
   theme: {
     extend: {
