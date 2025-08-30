def4f3d08cfa1db236c933f24dfb1ddb143bf85b
Alex Arguelles <alexarguelles87923@gmail.com>
2025-08-30 17:18:50 -0400

    changing hero

--- WebServer/src/app/portfolio/hero/hero.component.ts
+++ WebServer/src/app/portfolio/hero/hero.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css'],
   standalone: true
 })
 export class HeroComponent { }
